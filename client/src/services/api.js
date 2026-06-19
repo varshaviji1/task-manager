b318f4d311eebc171fa53c5aa9ed5c6377e8afca
varshaviji1 <vijayasrisaravanan21@gmail.com>
2026-06-19 22:49:27 +0530

    Added baseurl

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://student-task-manager-jyjf.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://task-manager-wr56.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
